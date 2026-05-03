f2fec171792263a0b7ee4fb68e9efeec7d9e4a6a
adrianaaguilar-hub <adriana.aguilar@alumnos.uneatlantico.es>
2026-05-03 22:18:55 +0200

    feat: clase Carta

--- src/Carta.java
+++ src/Carta.java
@@ -1,12 +1,47 @@
 public class Carta {
+    private int numero;
+    private int palo;
 
     public Carta(int palo, int numero) {
-        //TODO Auto-generated constructor stub
+        this.palo = palo;
+        this.numero = numero;
     }
 
     public void mostrar() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'mostrar'");
+        System.out.println(obtenerNumero() + "de " + obtenerPalo());
     }
+
+    public String obtenerNumero () {
+        String resultado = "";
+        if(numero == 0) {
+            resultado = "A";
+        } else if(numero == 11) {
+            resultado = "J";
+        } else if(numero == 12) {
+            resultado = "Q";
+        } else if(numero == 13) {
+            resultado = "K";
+        } else {
+            resultado = "" + (numero + 1);  
+        }
+        return resultado;
+    }
+    
+    private String obtenerPalo() {
+        String resultado = "";
+        if(palo == 0) {
+            resultado = "♠ (Picas)";
+        } else if(palo == 1) {
+            resultado = "♥ (Corazones)";
+        } else if(palo == 2) {
+            resultado = "♦ (Diamantes)";
+        } else if(palo == 3) {
+            resultado = "♣ (Tréboles)";
+        } else {
+            resultado = "Desconocido";
+        }
+        return resultado;
+    }
+
 
 }
