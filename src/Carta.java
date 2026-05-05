ebcc73c96b9bc4287868ecdacfc811e002412937
manuel@imac.xfce <manuel@masiasweb.com>
2026-05-05 10:54:19 +0200

    feat: clase Carta y sus responsabilidades

--- src/Carta.java
+++ src/Carta.java
@@ -1,17 +1,32 @@
 public class Carta {
 
+    private int palo;
+    private int numero;
+    private boolean bocaArriba;
+
+    private final String[] PALOS = { "♠️", "♣️", "♦️", "♥️" };
+    private final String[] NUMEROS = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "X", "J", "Q", "K" };
+
+    private Console console;
+
     public Carta(int palo, int numero) {
-        //TODO Auto-generated constructor stub
+        this.palo = palo;
+        this.numero = numero;
+        bocaArriba = false;
+        console = new Console();
     }
 
     public void mostrar() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'mostrar'");
+        String numero = "?";
+        String palo = "?";
+        if (this.bocaArriba) {
+            numero = NUMEROS[this.numero];
+            palo = PALOS[this.palo];
+        }
+        console.write("[" + numero + " " + palo + "]");
     }
 
     public void voltear() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'voltear'");
+        bocaArriba = !bocaArriba;
     }
-
 }
